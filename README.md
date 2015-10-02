c1a5fdd778cde0f8466ecf1b21b60e5146411164
Ian Hopkinson <ian@scraperwiki.com>
2015-10-02 15:34:10 +0100

    Try to pull data from NewsReader - get an ssl error

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 
  A web app which demos the [NewsReader Project](https://) technology by making visualisations via the [Simple API](), developed at [ScraperWiki](). 
 
- 
+ The actor graph visualisation started life with this:
+
+ http://bl.ocks.org/mbostock/4062045
--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from flask import render_template
 from app import app
 
 import json
+import os
+import requests
 
 data = []
 
@@ -23,5 +25,28 @@ def read_data_file(filename):
     with open(filename) as data_file:    
         data = json.load(data_file)
     return data
-    
-data = read_data_file("app/static/miserables.json") 
+
+def call_newsreader_events_shared(name):
+    api_key = os.environ['NEWSREADER_PUBLIC_API_KEY']
+    data = {"nodes": [], "links": []}
+    names = [name]
+    data["nodes"].append({"name":names[0],"group":1})
+    query_string_template = "https://newsreader.scraperwiki.com/wikinews/people_sharing_event_with_a_person/page/{page}?uris.0=dbpedia%3A{name}&output=json&api_key={api_key}"
+    index = 1
+    for name in names:
+        page = 1
+        while True:
+            r = requests.get(query_string_template.format(name=name, page=page, api_key=api_key))
+            page = page + 1
+            if "error" in r.json:
+                break
+
+            actor_data = r.json()
+            for entry in actor_data["payload"]:
+                data["nodes"].append({"name":entry["actor2"],"group":1})
+                data["links"].append({"source":index,"target":0,"value":1},)
+                index = index + 1
+
+    return data
+
+data = call_newsreader_events_shared("Barack_Obama") 
