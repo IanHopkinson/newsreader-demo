10acf33b1f45b4bcdecdce666fd3d68474fdc9f8
Ian Hopkinson <ian@scraperwiki.com>
2015-10-02 14:46:47 +0100

    Add a D3 network visualisation, one of Mike Bostock's in the first instance

--- app/views.py
+++ app/views.py
@@ -5,7 +5,23 @@ from flask import render_template
 
 from app import app
 
-@app.route('/', methods=['GET', 'POST'])
+import json
+
+data = []
+
+@app.route('/')
 def index():
     content = render_template("index.html")
     return content
+
+@app.route('/data')
+def getData():
+    global data
+    return json.dumps(data)
+
+def read_data_file(filename):
+    with open(filename) as data_file:    
+        data = json.load(data_file)
+    return data
+    
+data = read_data_file("app/static/miserables.json") 
