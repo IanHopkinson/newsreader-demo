ed5ef4042a0e702537dc43f1114d56fd373d8107
Ian Hopkinson <ian@scraperwiki.com>
2015-10-13 10:47:18 +0100

    Refactoring network.js

--- app/static/network.js
+++ app/static/network.js
@@ -1,5 +1,12 @@
 var data;
 var color = d3.scale.category20();
+var width = 600, height = 400;
+
+var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(100)
+        .size([width, height]);
+
 
 function nodeClick(d) {
     $("#selected-actor").html('<strong>' + d.name + ':  </strong>')
@@ -11,7 +18,54 @@ function nodeClick(d) {
 }
 
 function nodeDblclick() {
-    console.log("dblclick" + d3.select(this).title)
+    var actor = d3.select(this).select("title").text()
+    var endpoint = $('#endpoint').text()
+    console.log("Getting data for " + actor + " from " + endpoint)
+    // Get data for new node
+    d3.json("/network/" + endpoint + "/" + actor + "/data", function(error, new_graph) {
+        if (error) throw error;
+        new_data = new_graph;
+        data.nodes.push(new_graph.nodes)
+        data.links.push(new_graph.links)
+    // Overlay new node data on old
+        console.log("length of new data for : " + actor + " is " + new_data.nodes.length)
+        force
+            .nodes(data.nodes)
+            .links(data.links)
+            .start();
+
+        var svg = d3.select(".container")
+        var link = svg.selectAll(".link")
+            .data(data.links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function(d) {
+                return Math.sqrt(d.value);
+            });
+
+        var node = svg.selectAll(".node")
+            .data(data.nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return 'translate(' + [d.x, d.y] + ')'; })
+            .on("click", nodeClick)
+            .on("dblclick", nodeDblclick)
+            .call(force.drag);
+
+        node.append("circle")
+            .attr("r", function(d) {
+                return 3.0 + 2.0 * Math.sqrt(d.value);
+            })
+            .style("fill", function(d) {
+                return color(d.group);
+            })
+
+        node.append("title")
+            .text(function(d) {
+                return d.name;
+            });
+    });
+
 }
 
 function pressRefresh(e){
@@ -26,14 +80,6 @@ function pressRefresh(e){
 $(document).ready(function() {
     // Start fetching data
     $("#refresh-btn").addClass("loading").html('Fetching…')
-
-    var width = 600,
-        height = 400;
-
-    var force = d3.layout.force()
-        .charge(-120)
-        .linkDistance(100)
-        .size([width, height]);
 
     /* var zoom = d3.behavior.zoom()
         .scaleExtent([1, 10])
@@ -79,6 +125,7 @@ $(document).ready(function() {
             .data(graph.nodes)
             .enter().append("g")
             .attr("class", "node")
+            .attr("transform", function(d) { return 'translate(' + [d.x, d.y] + ')'; })
             .on("click", nodeClick)
             .on("dblclick", nodeDblclick)
             .call(force.drag);
@@ -117,12 +164,11 @@ $(document).ready(function() {
                 });
 
             node.attr("transform", function(d) { return 'translate(' + [d.x, d.y] + ')'; })
-
+        });
         // Put in the central actor name and biog
         $("#central-actor").html('<strong>' + actor + ':  </strong>')
         $("#central-actor-biog").html(graph["nodes"][0]["comment"])
         // Put the Refresh button back to normal
         $("#refresh-btn").removeClass("loading").html('<span class="glyphicon glyphicon-refresh"></span>Refresh')
-        });
     });
 });
